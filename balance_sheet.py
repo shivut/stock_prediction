98ecbd463102e6a0f954bfb43b61034c4adbc127
Shiva <shivut42@gmail.com>
2020-04-07 20:19:22 +0530

    added balance sheet

--- balance_sheet.py
+++ balance_sheet.py
@@ -1,110 +1,63 @@
-#!/usr/bin/python3
-
-import urllib.request
 from bs4 import BeautifulSoup
-import logging
 import os
-import sys
 import requests
-import csv
 import json
-
-def get_path():
-    # file_path = input('Please enter the path where you want to download data or enter "No" to exit: \n')
-    file_path = "/home/shiva/predict_stock/test/balance_sheet"
-
-    while file_path.upper() != 'NO':
-
-        if os.path.exists(file_path):
-            if not os.path.exists(os.path.join(file_path, 'data')):
-                os.makedirs(os.path.join(file_path, 'data'))
-            break
-        else:
-            file_path = input('Path not found... Enter the valid path or NO to exit: \n')
-
-            if file_path.upper() == 'NO':
-                sys.exit(0)
-
-    return os.path.join(file_path, 'balance_sheet')
+from get_historical_data import get_mappings_path
+from get_mappings import setup_logger
 
 
-def get_stock_data(sectorwise_com, company_code_mapping, path, logger):
+def get_balance_sheet(sectorwise_com, company_code_mapping, path, logger):
 
-    for key, value in sectorwise_com.items():
-        logger.debug(" **************** Downloading the data for the sector **************** " + str(key))
+    for each_sector, each_sector_companies in sectorwise_com.items():
+        logger.debug(" **************** Downloading the data for the sector **************** " + str(each_sector))
 
-        if value:
+        if each_sector_companies:
+            if not os.path.exists(os.path.join(path, each_sector)):
+                os.makedirs(os.path.join(path, each_sector))
 
-            if not os.path.exists(os.path.join(path, key)):
-                os.makedirs(os.path.join(path, key))
+            for each_com in each_sector_companies:
+                logger.debug("getting the balance sheet for " + each_com)
 
-            for each_com in value:
+                for each_division in range(1, 6):
 
-                print(each_com)
-                for eachDision in range(1,6):
+                    url = 'https://www.moneycontrol.com/financials/' + each_com.replace(" ", "") + '/balance-sheetVI/'\
+                          + company_code_mapping.get(each_com) + "/" + str(each_division)
+                    soup_obj = BeautifulSoup((requests.get(url)).content, 'html.parser')
 
-                    a = BeautifulSoup((requests.get(
-                        r'https://www.moneycontrol.com/financials/' + each_com.replace(" ", "") + '/balance-sheetVI/' + company_code_mapping.get(each_com) + "/" + str(eachDision))).content,
-                                      'html.parser')
-                    try:
-                        b = a.find_all(class_="mctable1")[0]
-                    except:
-                        break
+                    table_obj = soup_obj.find_all(class_="mctable1")
 
-                    e = b.find_all('table',
-                                   attrs={'cellpadding': "0", 'cellspacing': "0", 'class': "table4", 'width': "744"})
+                    if table_obj:
 
-                    with open(os.path.join(path, key, each_com + "_" + str(eachDision)+ '.txt'), 'w') as fl:
-                        for i in b.select('tr'):
-                            for j in i.find_all('td', attrs={'colspan': "2"}):
-                                break
-                            else:
+                        with open(os.path.join(path, each_sector, each_com + "_" + str(each_division) + '.txt'), 'w')\
+                                as fl:
+                            for i in table_obj[0].select('tr'):
                                 for k in i.find_all('td'):
                                     print(k.text, end='|', file=fl)
-                            print('\n', file=fl)
-
+                                print('\n', file=fl)
+                    else:
+                        break
 
     return True
 
 
-def setup_logger(path, name='downloadData', level=logging.DEBUG):
-    log_path = os.path.join(path, 'logs')
-
-    if not os.path.exists(log_path):
-        os.makedirs(log_path)
-
-    logger_path = os.path.join(log_path, 'download_data.log')
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-
-    terminal_handler = logging.StreamHandler()
-    terminal_handler.setFormatter(logging.Formatter("[%(levelname)s] %(asctime)s : %(message)s"))
-    terminal_handler.setLevel(logging.DEBUG)
-
-    file_handler = logging.FileHandler(logger_path, mode='w')
-    file_handler.setFormatter(logging.Formatter("[%(levelname)s] %(asctime)s %(filename)s:%(lineno)s : %(message)s"))
-    file_handler.setLevel(logging.DEBUG)
-
-    logger.addHandler(terminal_handler)
-
-    logger.addHandler(file_handler)
-    return logger
-
-
 if __name__ == '__main__':
 
-    path = get_path()
-    logger = setup_logger(path)
-    # sectorwise_com, company_code_mapping = get_sector_data(logger)
-    logger.debug("\n")
-    # sectorwise_com = {'Aluminium': ['Hindalco', 'Maan Aluminium', 'Manaksia Alumin', 'NALCO' ]}
-    # company_code_mapping = {'Century Extr': 'CE02', 'Hindalco': 'HI', 'Maan Aluminium': 'MA03',
-    #                         'Manaksia Alumin': 'MAC03', 'NALCO': 'NAC'}
-    sectorwise_com = json.loads(open("/home/shiva/predict_stock/sector_com_mapping.json").read().strip("\n"))
-    company_code_mapping= json.loads(open("/home/shiva/predict_stock/company_code_mapping.json").read().strip("\n"))
+    mapping_path = get_mappings_path()
 
-    logger.debug("Got the sector mapping and company code mapping")
-    is_success = get_stock_data(sectorwise_com, company_code_mapping, path, logger)
+    if mapping_path:
+        logger_obj = setup_logger(mapping_path)
+        logger_obj.debug("Got the sector mapping and company code mapping")
 
-    if (is_success):
-        logger.debug("Completed the Job")
+        param_files = ["company_code_mapping.json", "sectorwise_com.json", "sector_key_map.json"]
+        file_handler = []
+
+        for each_file in param_files:
+            file_handler.append(json.load(open(each_file)))
+
+        is_success = get_balance_sheet(file_handler[1], file_handler[0], mapping_path, logger_obj)
+
+        if is_success:
+            logger_obj.debug("Completed the job successfully")
+
+        for each_file in file_handler:
+            each_file.close()
